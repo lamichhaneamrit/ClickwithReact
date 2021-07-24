385d878eb256f9183f8dfc723ef96df07a7bb78f
lamichhaneamrit <lami.amrit@gmail.com>
2021-07-24 10:16:19 +0200

    first react app

---
+++ src/Application.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import HighScore from "./HighScore";
+import "./css/style.css";
+
+class Application extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0,
+        };
+    }
+
+    handleClick = () => {
+        this.setState({ count: this.state.count + 1 });
+    };
+    componentDidUpdate(props, state) {
+        if (
+            this.state.count > 10 &&
+            this.state.count != state.count &&
+            !this.state.overTen
+        ) {
+            console.log("updating over Ten");
+            this.setState({ overTen: true });
+        }
+    }
+
+    resetCount = (e) => {
+        this.setState({
+            count: 0,
+            overTen: false,
+        });
+    };
+    render() {
+        let { count } = this.state;
+
+        return ( <
+            div >
+            <
+            h1 > { " " }
+            Hello, You have clicked { count }
+            times { " " } <
+            /h1>{" "} <
+            HighScore overTen = { this.state.overTen }
+            onReset = { this.resetCount }
+            />{" "} <
+            span >
+            <
+            button onClick = {
+                () => this.handleClick() } > Click Me < /button>{" "} <
+            /span>{" "} <
+            /div>
+        );
+    }
+}
+
+export default Application;
